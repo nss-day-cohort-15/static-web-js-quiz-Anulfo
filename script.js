736910c17267a377568ea337c9d9fd3167f847d4
Anulfo <noaharkling@gmail.com>
2016-07-24 20:11:38 -0500

    HTML and Engine Done

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
-var spc = [];
-var leafs = [];
-var finaltree = []
+//(function () {
+//     var spc = [];
+//     var leafs = [];
+//     var finaltree = []
 
-for (i = 9; i >=0; i--) {
-  spc.push(' '.repeat(i));
-}
-//console.log(spc);
+//     for (i = fileHeight; i >=0; i--) {
+//         spc.push(' '.repeat(i));
+//     }
+// //console.log(spc);
 
-for (j = 1; j < 20; j = j + 2) {
-  leafs.push('*'.repeat(j));
+//     for (j = 1; j < (fileHeight * 2); j = j + 2) {
+//         leafs.push(madeOf.repeat(j));
+//     }
+// //console.log(leafs);
+//     for (z = 0; z < 10; z++) {
+//         finaltree.push(spc[z], leafs[z]);
+//         console.log(finaltree.join(' '));
+//         finaltree = [];
+// }
+// }())
+var tester = function() {
+    console.log('Button Pushed');
+    var fileHeight = document.getElementById('heightNumber').value;
+    console.log(fileHeight);
+    var madeOf = document.getElementById('charType').value;
+    console.log(madeOf);
+        (function () {
+     var spc = [];
+     var leafs = [];
+     var finaltree = []
+
+     for (i = fileHeight; i >=0; i--) {
+         spc.push(' '.repeat(i));
+     }
+ //console.log(spc);
+
+     for (j = 1; j < (fileHeight * 2); j = j + 2) {
+         leafs.push(madeOf.repeat(j));
+    }
+ //console.log(leafs);
+     for (z = 0; z < 10; z++) {
+         finaltree.push(spc[z], leafs[z]);
+         console.log(finaltree.join(' '));
+         finaltree = [];
+ }
+ }())
 }
-//console.log(leafs);
-for (z = 0; z < 10; z++) {
-  finaltree.push(spc[z], leafs[z]);
-  console.log(finaltree.join(' '));
-  finaltree = [];
-}
+var butt = document.getElementById('fertilizer').addEventListener('click', tester);
