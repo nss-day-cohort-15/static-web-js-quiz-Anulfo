59a43ec3d5b216863ab4744b7434ea901aa1f086
Anulfo <noaharkling@gmail.com>
2016-07-25 10:55:14 -0500

    final version of treegrow

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-//(function () {
-//     var spc = [];
-//     var leafs = [];
-//     var finaltree = []
-
-//     for (i = fileHeight; i >=0; i--) {
-//         spc.push(' '.repeat(i));
-//     }
-// //console.log(spc);
-
-//     for (j = 1; j < (fileHeight * 2); j = j + 2) {
-//         leafs.push(madeOf.repeat(j));
-//     }
-// //console.log(leafs);
-//     for (z = 0; z < 10; z++) {
-//         finaltree.push(spc[z], leafs[z]);
-//         console.log(finaltree.join(' '));
-//         finaltree = [];
-// }
-// }())
 var tester = function() {
     console.log('Button Pushed');
     var fileHeight = document.getElementById('heightNumber').value;
@@ -38,11 +18,24 @@ var tester = function() {
          leafs.push(madeOf.repeat(j));
     }
  //console.log(leafs);
-     for (z = 0; z < 10; z++) {
+     for (z = 0; z < fileHeight; z++) {
          finaltree.push(spc[z], leafs[z]);
          console.log(finaltree.join(' '));
          finaltree = [];
  }
  }())
 }
+var eRoutine = function (e) {
+    console.log(e.keyCode)
+    if (e.keyCode == 13  && (inputHeight != 0 || inputChar != 0)) {
+        console.log(tester)
+        tester();
+    }
+    // else {
+    //     alert('Press Enter after filling the spaces')
+    // }
+}
+var inputHeight = document.getElementById('heightNumber');
+var inputChar = document.getElementById('charType');
 var butt = document.getElementById('fertilizer').addEventListener('click', tester);
+var enterK = document.addEventListener('keydown', eRoutine);
